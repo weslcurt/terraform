5a91f9a8a101810bdbddde250d6c6712878dc9e9
weslcurt <18757779+weslcurt@users.noreply.github.com>
2020-05-15 21:13:58 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+variable "pm_api_url" {
+  default = "https://<cluster_ip>:8006/api2/json"
+}
+
+variable "pm_user" {
+default = "<root_user>@pam"
+}
+
+variable "pm_password" {
+default = "<password>"
+}
